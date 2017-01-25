3c61b2ba90100b75b18fed8b3eb8777675a5f947
Sebastian Straub <sebastian-straub@gmx.net>
2017-01-25 14:59:40 +0100

    first working version - replaces old stations

---
+++ mod.lua
@@ -0,0 +1,11 @@
+function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "NONE",
+		name = _("More Tracks for Stations"),
+		description = _("desc"),
+	}
+}
+end
---
+++ strings.lua
@@ -0,0 +1,11 @@
+function data()
+return {
+	en = {
+		desc = "Increases the number of available Tracks for all vanilla train stations."
+	},
+	de = {
+		["More Tracks for Stations"] = "Mehr Gleise für Bahnhöfe",
+		desc = "Erhöht die Anzahl der verfügbaren Gleise für alle Vanilla-Bahnhöfe."
+	}
+}
+end
